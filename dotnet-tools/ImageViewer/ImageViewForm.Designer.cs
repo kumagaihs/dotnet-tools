4016b1b0dcbcbb9049818bae309b958cee759220
t_kumagaihs <t_kumagaihs@local>
2021-09-17 18:58:29 +0900

    ImageViewer作成

---
+++ dotnet-tools/ImageViewer/ImageViewForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace ImageViewer
+{
+    partial class ImageView
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileTreeView = new System.Windows.Forms.TreeView();
+            this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.baseDirTextBox = new System.Windows.Forms.TextBox();
+            this.baseDirResetButton = new System.Windows.Forms.Button();
+            this.picHeightLabel = new System.Windows.Forms.Label();
+            this.picWidthLabel = new System.Windows.Forms.Label();
+            this.picHeightTextBox = new System.Windows.Forms.TextBox();
+            this.picWidthTextBox = new System.Windows.Forms.TextBox();
+            this.thumbnailPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
+            this.mainSplitContainer.Panel1.SuspendLayout();
+            this.mainSplitContainer.Panel2.SuspendLayout();
+            this.mainSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileTreeView
+            // 
+            this.fileTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileTreeView.Location = new System.Drawing.Point(0, 30);
+            this.fileTreeView.Name = "fileTreeView";
+            this.fileTreeView.Size = new System.Drawing.Size(204, 428);
+            this.fileTreeView.TabIndex = 0;
+            this.fileTreeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.fileTreeBiew_BeforeExpand);
+            this.fileTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.fileTreeView_AfterSelect);
+            this.fileTreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.fileTreeView1_NodeMouseClick);
+            // 
+            // mainSplitContainer
+            // 
+            this.mainSplitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainSplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.mainSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.mainSplitContainer.Name = "mainSplitContainer";
+            // 
+            // mainSplitContainer.Panel1
+            // 
+            this.mainSplitContainer.Panel1.Controls.Add(this.baseDirTextBox);
+            this.mainSplitContainer.Panel1.Controls.Add(this.baseDirResetButton);
+            this.mainSplitContainer.Panel1.Controls.Add(this.fileTreeView);
+            // 
+            // mainSplitContainer.Panel2
+            // 
+            this.mainSplitContainer.Panel2.Controls.Add(this.picHeightLabel);
+            this.mainSplitContainer.Panel2.Controls.Add(this.picWidthLabel);
+            this.mainSplitContainer.Panel2.Controls.Add(this.picHeightTextBox);
+            this.mainSplitContainer.Panel2.Controls.Add(this.picWidthTextBox);
+            this.mainSplitContainer.Panel2.Controls.Add(this.thumbnailPanel);
+            this.mainSplitContainer.Size = new System.Drawing.Size(842, 460);
+            this.mainSplitContainer.SplitterDistance = 206;
+            this.mainSplitContainer.TabIndex = 2;
+            // 
+            // baseDirTextBox
+            // 
+            this.baseDirTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.baseDirTextBox.Location = new System.Drawing.Point(0, 0);
+            this.baseDirTextBox.MinimumSize = new System.Drawing.Size(4, 30);
+            this.baseDirTextBox.Name = "baseDirTextBox";
+            this.baseDirTextBox.Size = new System.Drawing.Size(149, 30);
+            this.baseDirTextBox.TabIndex = 2;
+            // 
+            // baseDirResetButton
+            // 
+            this.baseDirResetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.baseDirResetButton.Location = new System.Drawing.Point(150, 0);
+            this.baseDirResetButton.Name = "baseDirResetButton";
+            this.baseDirResetButton.Size = new System.Drawing.Size(53, 30);
+            this.baseDirResetButton.TabIndex = 1;
+            this.baseDirResetButton.Text = "reload";
+            this.baseDirResetButton.UseVisualStyleBackColor = true;
+            this.baseDirResetButton.Click += new System.EventHandler(this.baseDirResetButton_Click);
+            // 
+            // picHeightLabel
+            // 
+            this.picHeightLabel.AutoSize = true;
+            this.picHeightLabel.Location = new System.Drawing.Point(105, 9);
+            this.picHeightLabel.Name = "picHeightLabel";
+            this.picHeightLabel.Size = new System.Drawing.Size(38, 12);
+            this.picHeightLabel.TabIndex = 3;
+            this.picHeightLabel.Text = "Height";
+            // 
+            // picWidthLabel
+            // 
+            this.picWidthLabel.AutoSize = true;
+            this.picWidthLabel.Location = new System.Drawing.Point(3, 9);
+            this.picWidthLabel.Name = "picWidthLabel";
+            this.picWidthLabel.Size = new System.Drawing.Size(33, 12);
+            this.picWidthLabel.TabIndex = 3;
+            this.picWidthLabel.Text = "Width";
+            // 
+            // picHeightTextBox
+            // 
+            this.picHeightTextBox.Location = new System.Drawing.Point(149, 0);
+            this.picHeightTextBox.MinimumSize = new System.Drawing.Size(4, 30);
+            this.picHeightTextBox.Name = "picHeightTextBox";
+            this.picHeightTextBox.Size = new System.Drawing.Size(49, 19);
+            this.picHeightTextBox.TabIndex = 2;
+            // 
+            // picWidthTextBox
+            // 
+            this.picWidthTextBox.Location = new System.Drawing.Point(41, 0);
+            this.picWidthTextBox.MinimumSize = new System.Drawing.Size(4, 30);
+            this.picWidthTextBox.Name = "picWidthTextBox";
+            this.picWidthTextBox.Size = new System.Drawing.Size(49, 19);
+            this.picWidthTextBox.TabIndex = 2;
+            // 
+            // thumbnailPanel
+            // 
+            this.thumbnailPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.thumbnailPanel.AutoScroll = true;
+            this.thumbnailPanel.BackColor = System.Drawing.Color.White;
+            this.thumbnailPanel.Location = new System.Drawing.Point(0, 30);
+            this.thumbnailPanel.Name = "thumbnailPanel";
+            this.thumbnailPanel.Size = new System.Drawing.Size(630, 428);
+            this.thumbnailPanel.TabIndex = 0;
+            // 
+            // ImageView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 461);
+            this.Controls.Add(this.mainSplitContainer);
+            this.Name = "ImageView";
+            this.Text = "ImageView";
+            this.mainSplitContainer.Panel1.ResumeLayout(false);
+            this.mainSplitContainer.Panel1.PerformLayout();
+            this.mainSplitContainer.Panel2.ResumeLayout(false);
+            this.mainSplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
+            this.mainSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView fileTreeView;
+        private System.Windows.Forms.SplitContainer mainSplitContainer;
+        private System.Windows.Forms.Button baseDirResetButton;
+        private System.Windows.Forms.TextBox baseDirTextBox;
+        private System.Windows.Forms.Panel thumbnailPanel;
+        private System.Windows.Forms.Label picHeightLabel;
+        private System.Windows.Forms.Label picWidthLabel;
+        private System.Windows.Forms.TextBox picHeightTextBox;
+        private System.Windows.Forms.TextBox picWidthTextBox;
+    }
+}
+
---
+++ dotnet-tools/ImageViewer/ImageViewForm.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class ImageView : Form
+    {
+        public ImageView()
+        {
+            InitializeComponent();
+
+            // サムネイルサイズを設定
+            picWidthTextBox.Text = "200";
+            picHeightTextBox.Text = "200";
+
+            // ドライブ一覧を走査してツリーに追加
+            reloadFileTreeView(Environment.GetLogicalDrives());
+
+            // XXX : Debug
+//            reloadFileTreeView(new string[] { @"C:\work" });
+
+        }
+
+        /// <summary>
+        /// ツリービューを指定されたパスをベースに更新する。
+        /// </summary>
+        /// <param name="paths">ツリービューのベースにするパス（複数指定可能）</param>
+        private void reloadFileTreeView(String[] paths)
+        {
+            // ツリーをクリア
+            fileTreeView.Nodes.Clear();
+
+            // パスをツリーに追加
+            foreach (String path in paths)
+            {
+                // 新規ノード作成
+                // プラスボタンを表示するため空のノードを追加しておく
+                TreeNode node = new TreeNode(path);
+                node.Nodes.Add(new TreeNode());
+                fileTreeView.Nodes.Add(node);
+            }
+        }
+
+        /// <summary>
+        /// ツリービュー項目展開時（前）のイベントハンドラ.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fileTreeBiew_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            String path = node.FullPath;
+            node.Nodes.Clear();
+
+            try
+            {
+                DirectoryInfo dirList = new DirectoryInfo(path);
+                foreach (DirectoryInfo di in dirList.GetDirectories())
+                {
+                    TreeNode child = new TreeNode(di.Name);
+                    child.Nodes.Add(new TreeNode());
+                    node.Nodes.Add(child);
+                }
+            }
+            catch (IOException ie)
+            {
+                MessageBox.Show(ie.Message, "選択エラー");
+            }
+
+        }
+
+        private void fileTreeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            reloadThumbnail(e.Node.FullPath);
+        }
+
+        private void fileTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void baseDirResetButton_Click(object sender, EventArgs e)
+        {
+            reloadFileTreeView(new String[] { baseDirTextBox.Text });
+        }
+
+        private void reloadThumbnail(String dir)
+        {
+            // サムネイルペインをクリア
+            thumbnailPanel.Controls.Clear();
+
+            // ファイル一覧取得
+            List<String> files = Directory.GetFiles(dir).ToList<String>();
+
+            // サムネイル表示
+            Task task = Task.Run(() => {
+                int thumWidth = int.Parse(picWidthTextBox.Text);
+                int thumHeight = int.Parse(picHeightTextBox.Text);
+                int marginX = 1;
+                int marginY = 1;
+                int col = 0;
+                int row = 0;
+                foreach (String file in files) {
+                    try {
+                        PictureBox pic = createPictureBox(file, col * (thumWidth + marginX), row * (thumHeight + marginY), thumWidth, thumHeight);
+                        Invoke(new Action(() =>
+                        {
+                            thumbnailPanel.Controls.Add(pic);
+                        }));
+                    }
+                    catch (Exception e)
+                    {
+                        // Do nothing.
+                    }
+
+                    if (((col + 2) * (thumWidth + marginX)) > thumbnailPanel.Width) {
+                        col = 0;
+                        row++;
+                    }
+                    else {
+                        col++;
+                    }
+                }
+            });
+        }
+
+        private Object lockObject = new Object();
+
+        private PictureBox createPictureBox(String file, int x, int y, int width, int height)
+        {
+            lock (lockObject)
+            {
+                // PictureBoxの生成
+                PictureBox pic = new PictureBox();
+                pic.Text = file;
+                pic.Location = new Point(x, y);
+                pic.Size = new System.Drawing.Size(width, height);
+                pic.MouseClick += thumbnalePictureBox_MouseClick;
+
+                // 画像ファイルの読み込み
+                Bitmap canvas = new Bitmap(pic.Width, pic.Height);
+                Graphics g = Graphics.FromImage(canvas);
+                Image img = Image.FromFile(file);
+
+                // 補間方法として高品質双三次補間を指定する
+                g.InterpolationMode =
+                    System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                // サムネイル画像サイズに合わせて縮小する
+                Rectangle srcRect = new Rectangle(0, 0, pic.Width, pic.Height);
+                // 縦長の画像
+                if (img.Width < img.Height)
+                {
+                    // 横幅を基準にサイズ合わせした場合の高さを算出
+                    int resizeHeight = (int)(img.Height * ((double)pic.Width / (double)img.Width));
+
+                    // 表示領域をオーバーする高さを算出
+                    int over = resizeHeight - pic.Height;
+                    over = (int)(over * ((double)img.Width / (double)pic.Width));
+
+                    // 横幅はそのままで、高さは中央を表示するように上下をトリミング
+                    srcRect = new Rectangle(
+                        0,
+                        over / 2,
+                        img.Width,
+                        img.Height - (over)
+                    );
+                }
+                // 横長の画像
+                else if (img.Height < img.Width)
+                {
+                    // 高さを基準にサイズ合わせした場合の横幅を算出
+                    int resizeWidth = (int)(img.Width * ((double)pic.Height / (double)img.Height));
+
+                    // 表示領域をオーバーする横幅を算出
+                    int over = resizeWidth - pic.Width;
+                    over = (int)(over * ((double)img.Height / (double)pic.Height));
+
+                    // 高さはそのままで、横幅は中央を表示するように左右をトリミング
+                    srcRect = new Rectangle(
+                        over / 2,
+                        0,
+                        img.Width - (over),
+                        img.Height
+                    );
+                }
+
+                // 画像のサイズを縮小・トリミングしてcanvasに描画する
+                g.DrawImage(img, new Rectangle(0, 0, pic.Width, pic.Height), srcRect, GraphicsUnit.Pixel);
+
+                // リソースを解放する
+                img.Dispose();
+                g.Dispose();
+
+                // pic.Image = new System.Drawing.Bitmap(file);
+                pic.Image = canvas;
+
+                return pic;
+            }
+        }
+
+        private void thumbnalePictureBox_MouseClick(Object sender, MouseEventArgs e)
+        {
+            // PictureBoxの生成
+            PictureBox pic = new PictureBox();
+            // pic.BackColor = Color.Transparent;
+            pic.Location = new Point(0, 0);
+            pic.Size = new System.Drawing.Size(this.Size.Width, this.Size.Height);
+            pic.MouseClick += fullPictureBox_MouseClick;
+
+            // 画像ファイルの読み込み
+            Bitmap canvas = new Bitmap(pic.Width, pic.Height);
+            Graphics g = Graphics.FromImage(canvas);
+            Image img = Image.FromFile(((PictureBox)sender).Text);
+
+            // 補間方法として高品質双三次補間を指定する
+            g.InterpolationMode =
+                System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            // 画像サイズを拡大 or 縮小
+            int width = 0;
+            int height = 0;
+            if ((double)img.Width / img.Height < (double)pic.Width / pic.Height) {
+                // 表示領域の方が横長なら、縦方向はいっぱいに表示
+                width = (int)(pic.Height * ((double)img.Width / (double)img.Height));
+                height = pic.Height;
+            }
+            else if (img.Height < img.Width) {
+                // 画像の方が横長なら、横方向はいっぱいに表示
+                width = pic.Width;
+                height = (int)(pic.Width * ((double)img.Height / (double)img.Width));
+            }
+
+            // 画像表示
+            g.DrawImage(img, 0, 0, width, height);
+
+            // PictureBoxを画像サイズぴったりにリサイズ
+            pic.Width = width;
+            pic.Height = height;
+
+            // リソースを解放する
+            img.Dispose();
+            g.Dispose();
+
+            // 表示
+            pic.Image = canvas;
+            this.Controls.Add(pic);
+
+            // 画像を最前面に表示
+            pic.BringToFront();
+        }
+
+        private void fullPictureBox_MouseClick(Object sender, MouseEventArgs e)
+        {
+            this.Controls.Remove(((PictureBox)sender));
+        }
+    }
+}
